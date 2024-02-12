16a8dc546927d4f957c21c0de8d7fd642032e3e0
Grigoriy Popov <grisha_mgn@bk.ru>
2024-02-12 16:12:09 +0300

    add delete task button

--- js/sctipt.js
+++ js/sctipt.js
@@ -1,7 +1,34 @@
+function createTask() {
+    let taskTitleContent = document.querySelector(".add-task__input").value;
+
+    let task = document.createElement("li");
+    task.classList.add("tasks__task", "task");
+
+    let taskTitle = document.createElement("h3");
+    taskTitle.classList.add("task__title");
+    taskTitle.textContent = taskTitleContent;
+    task.append(taskTitle);
+
+    let taskEditButton = document.createElement("button");
+    taskEditButton.classList.add("task__edit-btn");
+    taskEditButton.textContent = "E";
+    task.append(taskEditButton)
+
+    let taskDeleteButton = document.createElement("button");
+    taskDeleteButton.classList.add("task__delete-btn");
+    taskDeleteButton.textContent = "X";
+    taskDeleteButton.onclick = function() {
+        taskDeleteButton.parentElement.remove();
+    }
+    task.append(taskDeleteButton)
+
+    return task;
+}
+
 function addTask() {
-    let taskContent = document.querySelector(".add-task__input").value;
     let taskList = document.querySelector(".tasks__list");
-    let task = document.createElement("div");
-    task.textContent = taskContent;
+
+    let task = createTask();
+
     taskList.append(task);
 }
