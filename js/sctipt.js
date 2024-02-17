5e4eee90dc0c8667d3af10b2f99839d8bb74a836
Grigoriy Popov <grisha_mgn@bk.ru>
2024-02-17 22:24:37 +0300

    add edit popup and add task by press the enter

--- js/sctipt.js
+++ js/sctipt.js
@@ -1,7 +1,16 @@
 function createTask(inputValue, descriptionValue = "") {
     // insertadjacentText
     const task = document.createElement("li");
-    task.classList.add("tasks__task", "task");
+    task.classList.add("tasks__task", "task", "open-popup");
+    task.dataset.popup = "edit-task-popup";
+    task.addEventListener("click", function(e) {
+        if (!e.target.classList.contains('task__delete-btn')) {
+            task.classList.add("selected");
+            const popup = document.getElementById("edit-task-popup");
+            constructPopup(task);
+            openPopup(popup);
+        }
+    });
 
     const header = document.createElement("div");
     header.classList.add("task__header");
@@ -20,7 +29,7 @@ function createTask(inputValue, descriptionValue = "") {
     taskDeleteButton.classList.add("task__delete-btn");
     taskDeleteButton.textContent = "X";
     taskDeleteButton.onclick = function() {
-        taskDeleteButton.parentElement.remove();
+        taskDeleteButton.closest(".task").remove();
     }
     header.append(taskTitle, taskDate, taskDeleteButton);
 
@@ -70,13 +79,21 @@ let unlock = true;
 
 const openElements = document.querySelectorAll(".open-popup");
 for (let openElement of openElements) {
-    if (openElement.dataset.popup != "add-task-popup") {
+    if (openElement.dataset.popup === "add-task-popup") {
+        openElement.addEventListener("click", addTask);
+    } else if (openElement.dataset.popup === "edit-task-popup") {
+
+        openElement.addEventListener("click", function() {
+            openElement.classList.add("selected");
+            const popup = document.getElementById(`${openElement.dataset.popup}`);
+            constructPopup(openElement);
+            openPopup(popup);
+        });
+    } else {
         openElement.addEventListener("click", function() {
             const popup = document.getElementById(`${openElement.dataset.popup}`);
             openPopup(popup);
         })
-    } else {
-        openElement.addEventListener("click", addTask);
     }
 }
 
@@ -87,6 +104,12 @@ for (let openElement of openElements) {
 //     })
 // }
 
+function constructPopup(task) {
+    const editPopup = document.getElementById("edit-task-popup");
+    editPopup.querySelector(".popup__task-title").value = task.querySelector(".task__title").textContent;
+    editPopup.querySelector(".popup__task-description").value = task.querySelector(".task__description").textContent;
+
+}
 
 
 function openPopup(currentPopup) {
@@ -109,6 +132,7 @@ function openPopup(currentPopup) {
         // }
     
         if (closeElement.id === "add-task-popup-btn") closeElement.addEventListener("click", addTaskHandler);
+        if (closeElement.id === "edit-task-popup-btn") closeElement.addEventListener("click", editTaskHandler);
         else closeElement.addEventListener("click", closePopupHandler);
     }
     
@@ -142,6 +166,17 @@ function addTaskHandler(event) {
     closePopupHandler(event);
 }
 
+function editTaskHandler(event) {
+    const selectedTask = document.querySelector(".selected");
+    if (selectedTask) {
+        const editTaskPopup = event.target.closest("#edit-task-popup");
+        selectedTask.querySelector(".task__title").textContent = editTaskPopup.querySelector(".popup__task-title").value;
+        selectedTask.querySelector(".task__description").textContent = editTaskPopup.querySelector(".popup__task-description").value;
+    }
+    closePopupHandler(event);
+}
+
+
 function closePopup(currentPopup) {
     currentPopup.classList.remove("open");
 
@@ -149,6 +184,13 @@ function closePopup(currentPopup) {
     for (let closeElement of closeElements) {
         closeElement.removeEventListener("click", closePopupHandler);
         if (closeElement.id === "add-task-popup-btn") closeElement.removeEventListener("click", addTaskHandler);
+        if (closeElement.id === "edit-task-popup-btn") closeElement.removeEventListener("click", editTaskHandler);
     }
     currentPopup.removeEventListener("mousedown", outsidePopupClickHandler);
 }
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        addTask();
+    }
+})
